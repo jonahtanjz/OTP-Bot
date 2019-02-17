04bee5ae0cf43d61eb6aca3bfdf4c16b3e652eda
jonahtanjz <47470981+jonahtanjz@users.noreply.github.com>
2019-02-17 21:40:16 +0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,33 @@
+<?php
+
+class db 
+{
+	private static function dbc()
+	{	
+		$dsn = 'mysql:host=127.0.0.1; dbname= Database_Name; charset=utf8';
+		$user = '';
+		$password = '';
+		
+		try 
+		{
+			$dbh = new PDO($dsn, $user, $password);
+			return $dbh;
+		} catch (PDOException $e) {echo $e->getMessage();}
+		
+	}
+	
+	
+	public static function query($statement, $names = array())
+	{
+		$query = self::dbc()->prepare($statement);
+		$query = execute($names);
+		
+		if (explode(' ',$statement)[0] == 'SELECT')
+		{
+			$value = $query->fetchAll();
+			return $value;
+		}
+	}
+}
+
+?>
